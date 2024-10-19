30c183319eff699a153efc34b0cc924d0f288c6f
sophyjen <162417528+sophyjen@users.noreply.github.com>
2024-10-19 22:31:55 +0530

    Update index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import ButtonCounter from './Components/buttoncounter';
-// import ToggleVisibility from './Components/toggle';
-// import FormCaptureDisplay from './Components/formdisp';
- import ListRender from './Components/listrender';
-//import FetchDataAPI from './Components/fetchdataAPI';
+import ButtonCounter from './Components/buttoncounter';
+import ToggleVisibility from './Components/toggle';
+import FormCaptureDisplay from './Components/formdisp';
+import ListRender from './Components/listrender';
+import FetchDataAPI from './Components/fetchdataAPI';
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-// root.render(<ButtonCounter />)
+root.render(<ButtonCounter />)
 
-// root.render(<ToggleVisibility />)
+root.render(<ToggleVisibility />)
 
-// root.render(<FormCaptureDisplay />)
+root.render(<FormCaptureDisplay />)
 
- root.render(<ListRender />)
+root.render(<ListRender />)
 
-//root.render(<FetchDataAPI />)
+root.render(<FetchDataAPI />)
